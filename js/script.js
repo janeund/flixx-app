54a38047b36b8b2d6d58cb337f1e9234ddf75c9b
Evgeniia Zhavoronkova <maongaku@gmail.com>
2024-05-14 13:46:50 +0200

    feat: add display cards and fetch data functions

--- js/script.js
+++ js/script.js
@@ -2,6 +2,60 @@ const global = {
   currentPage: window.location.pathname,
 };
 
+async function displayPopularMovies() {
+  const { results } = await fetchAPIData('/movie/popular');
+  const cardsContainer = document.querySelector('#popular-movies');
+  results.forEach(movie => {
+    const movieEl = document.createElement('div');
+    movieEl.classList.add('card');
+    movieEl.innerHTML = `
+          <a href="movie-details.html?id=${movie.id}">
+            ${ 
+              movie.poster_path 
+              ? `<img
+              src="https://image.tmdb.org/t/p/w500${movie.poster_path}"
+              class="card-img-top"
+              alt="${movie.title}"
+            />` 
+            : `<img
+              src="images/no-image.jpg"
+              class="card-img-top"
+              alt="${movie.title}"
+            />`
+            }
+          </a>
+          <div class="card-body">
+            <h5 class="card-title"${movie.title}</h5>
+            <p class="card-text">
+              <small class="text-muted">${movie.release_date}</small>
+            </p>
+          </div>
+    `;
+    cardsContainer.appendChild(movieEl);
+  })
+}
+
+// Fetch data from API
+async function fetchAPIData(endpoint) {
+  const API_KEY = '005504cfb7e5160f459c7987f1017218';
+  const API_URL = 'https://api.themoviedb.org/3/';
+
+  const response = await fetch(`${API_URL}${endpoint}?api_key=${API_KEY}&language=en-US`);
+  const data = await response.json();
+  return data; 
+}
+
+
+// Highlight active link
+function highlightActiveLink() {
+  const links = document.querySelectorAll('.nav-link');
+  links.forEach(link => {
+    if (link.getAttribute('href') === global.currentPage) {
+      link.classList.add('active')
+    }
+  })
+}
+
 // Init App
 function init() {
   switch(global.currentPage) {
@@ -22,6 +76,9 @@ function init() {
       console.log('Search');
       break;
   }
+
+  highlightActiveLink();
+  displayPopularMovies()
 }
 
 document.addEventListener('DOMContentLoaded', init)
